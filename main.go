1884befb6d6955196732356ac5b32139625dcd60
Rakhim <rrakhimr@gmail.com>
2023-01-22 13:50:27 +0500

    commit

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func greating(response http.ResponseWriter, request *http.Request) {
 	if request.Method != http.MethodGet {
 		response.WriteHeader(http.StatusMethodNotAllowed)
 		fmt.Fprint(response, http.StatusText(http.StatusMethodNotAllowed)+"\n")
-		fmt.Print("Bala bla!")
+
 		return
 	}
 
