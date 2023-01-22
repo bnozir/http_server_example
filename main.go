1d2b6f8c2624150f616ec21d9ef089c0b51a1c29
Nozir Boqiev <bnozir.2797@gmail.com>
2023-01-22 12:15:29 +0500

    Some bug fixes

--- main.go
+++ main.go
@@ -20,8 +20,9 @@ func main() {
 func greating(response http.ResponseWriter, request *http.Request) {
 	if request.Method != http.MethodGet {
 		response.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprint(response, http.StatusText(http.StatusMethodNotAllowed))
+		fmt.Fprint(response, http.StatusText(http.StatusMethodNotAllowed)+"\n")
+		return
 	}
 
-	fmt.Fprint(response, "Hi, Bala!")
+	fmt.Fprint(response, "Hi, Bala!\n")
 }
