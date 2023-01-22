f2a28834766a407e40ee3648370dcc3e294c8842
Nozir Boqiev <bnozir.2797@gmail.com>
2023-01-22 11:28:28 +0500

    Simple server is ready

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hi", greating)
+
+	server := &http.Server{}
+	listener, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		panic(err)
+	}
+	server.Serve(listener)
+}
+
+func greating(response http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodGet {
+		response.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprint(response, http.StatusText(http.StatusMethodNotAllowed))
+	}
+
+	fmt.Fprint(response, "Hi, Bala!")
+}
